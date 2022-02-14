c9a48bb8e7de417c72231bc91baa17eaeb25a6e8
@danubiomuller <dudeful@outlook.com>
2022-02-14 12:10:54 +0000

    basic backend for MVP

---
+++ DesafioHard/backend/src/public/index.js
@@ -0,0 +1,56 @@
+const apiURL = 'http://edtech.dudeful.com:3004';
+
+$(document).ready(() => {
+	$('#save_score').click(saveScore);
+
+	renderRanking();
+});
+
+const saveScore = () => {
+	const name = $('#player_name').val();
+	const score = $('#player_score').val();
+	const data = { data: { name, score } };
+
+	$.post(apiURL + '/save-game', data)
+		.done((res) => {
+			console.log(res);
+			//CUIDADO!!!
+			//SE O PM2 ESTIVER COM A FLAG "--watch" ou A CONFIG "watch: true" O SERVIDOR
+			//SERÁ REINICIADO DURANTE O SALVAMENTO DO NOVO SCORE NO ARQUIVO JSON
+			//E PORTANTO, AO TENTAR CHAMAR A FUNÇÃO ABAIXO HAVERÁ UM ERRO, POIS
+			//A REQUISIÇÃO ACONTECERÁ NO MOMENTO EM QUE O SERVIDOR ESTÁ SENDO REINICIADO!
+			renderRanking();
+		})
+		.fail((error) => console.error(error));
+};
+
+const renderRanking = async () => {
+	if ($('#ranking')) $('#ranking').remove();
+
+	$('#root').append(`
+    <table id="ranking">
+      <tr>
+        <th>Jogador</th>
+        <th>Pontuação</th>
+        <th>Data</th>
+      </tr>
+    </table>
+  `);
+
+	try {
+		const response = await $.ajax(apiURL + '/ranking');
+
+		response.data.forEach((score) => {
+			$('#ranking').append(`
+        <tr>
+          <td>${score.name}</td>
+          <td>${score.score}</td>
+          <td>${score.date}</td>
+        </tr>
+      `);
+		});
+	} catch (error) {
+		alert('check console for errors');
+		console.error(error);
+	}
+};
