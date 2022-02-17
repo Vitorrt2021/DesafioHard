1473390875ffdacc0a98c15ad725311e94b084be
@danubiomuller <dudeful@outlook.com>
2022-02-17 13:10:06 +0000

    fix 'beforeunload' event

--- DesafioHard/frontend/src/gameplay_interface/gameplay_interface.js
+++ DesafioHard/frontend/src/gameplay_interface/gameplay_interface.js
@@ -1,39 +1,39 @@
-import Game from "../Components/Game.js";
-
-const preventReload = () => {
-  window.addEventListener("beforeunload", function (e) {
-    e.preventDefault();
-    // In Mozilla Firefox prompt will always be shown
-    // Chrome requires returnValue to be set
-    e.returnValue = "";
-  });
-};
+import Game from '../Components/Game.js';
 
 $(document).ready(function () {
-  $("#score_value").one("change", preventReload);
+	// Prevent user from reloading page by accident
+	window.addEventListener('beforeunload', function (e) {
+		e.preventDefault();
+		// In Mozilla Firefox prompt will always be shown
+		// Chrome requires returnValue to be set
+		e.returnValue = '';
+	});
 
-  $(".cat_tower").on("dragstart", function (event) {
-    event.originalEvent.dataTransfer.setData("text", "cat_tower_level_1");
-  });
+	$('.cat_tower').on('dragstart', function (event) {
+		event.originalEvent.dataTransfer.setData(
+			'text',
+			'cat_tower_level_1'
+		);
+	});
 
-  $(".blue_rabbit_tower").on("dragstart", function (event) {
-    event.originalEvent.dataTransfer.setData(
-      "text",
-      "blue_rabbit_tower_level_1"
-    );
-  });
+	$('.blue_rabbit_tower').on('dragstart', function (event) {
+		event.originalEvent.dataTransfer.setData(
+			'text',
+			'blue_rabbit_tower_level_1'
+		);
+	});
 
-  $(".red_rabbit_tower").on("dragstart", function (event) {
-    event.originalEvent.dataTransfer.setData(
-      "text",
-      "red_rabbit_tower_level_1"
-    );
-  });
+	$('.red_rabbit_tower').on('dragstart', function (event) {
+		event.originalEvent.dataTransfer.setData(
+			'text',
+			'red_rabbit_tower_level_1'
+		);
+	});
 
-  $("#canvas1").on("dragover", function (event) {
-    event.originalEvent.preventDefault();
-  });
+	$('#canvas1').on('dragover', function (event) {
+		event.originalEvent.preventDefault();
+	});
 
-  const game = new Game();
-  game.start();
+	const game = new Game();
+	game.start();
 });
