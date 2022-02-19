824ea50bedf66dd01b20308fd951f1db07e6d568
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-02-19 19:10:57 -0300

    Feat: showing the stats of the towers when I step over it

--- DesafioHard/frontend/src/gameplay_interface/gameplay_interface.js
+++ DesafioHard/frontend/src/gameplay_interface/gameplay_interface.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
 	// 	// Chrome requires returnValue to be set
 	// 	e.returnValue = '';
 	// });
+
 	$('.cat_tower').on('dragstart', function (event) {
 		event.originalEvent.dataTransfer.setData('text', 'cat_tower_level_1');
 	});
@@ -36,4 +37,26 @@ $(document).ready(function () {
 	$('canvas').click(() => {
 		game.evolveTower();
 	});
+
+	createTooltip('.blue_rabbit_tower', 750, 75, Math.floor(10000 / 170));
+	createTooltip('.red_rabbit_tower', 500, 100, Math.floor(10000 / 120));
+	createTooltip('.cat_tower', 500, 50, Math.floor(10000 / 200));
 });
+
+function createTooltip(element, live, strenght, speed) {
+	$(element).tooltip({
+		classes: {
+			'ui-tooltip': 'tooltip1',
+			'ui-tooltip-content': 'tooltip1',
+		},
+		content: `
+			<div class="tooltip1">
+				<img src="./images/strenght_icon.png" alt="Força">
+				<label id='strength_value'>${strenght}</label>
+				<img src="./images/live_icon.png" alt="Live">
+				<label id="live_value">${live}</label>
+				<img src="./images/speed_icon.png" alt="spped">
+				<label id="live_value">${speed}</label>
+			</div>`,
+	});
+}
