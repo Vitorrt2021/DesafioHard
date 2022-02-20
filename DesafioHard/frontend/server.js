e95463f23424932e39368fc120f085df12e704e4
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-02-20 16:53:05 -0400

    .

--- DesafioHard/frontend/server.js
+++ DesafioHard/frontend/server.js
@@ -4,36 +4,36 @@ const path = require('path');
 const port = 2500;
 const fs = require('fs');
 
-// app.use('/', express.static(path.join(__dirname, '/src/styles')));
-// app.use('/', express.static(path.join(__dirname, '/src/gameplay')));
-// app.use('/', express.static(path.join(__dirname, '/src')));
+app.use('/', express.static(path.join(__dirname, '/src/styles')));
+app.use('/', express.static(path.join(__dirname, '/src/gameplay')));
+app.use('/', express.static(path.join(__dirname, '/src')));
 
-// app.get('/get-assets', (req, res) => {
-// 	const data = getFilesInsideDirectory('./src/assets');
-// 	const data = getFilesInsideDirectory('../frontend/src/assets');
-// 	res.send(data);
-// });
+app.get('/get-assets', (req, res) => {
+	const data = getFilesInsideDirectory('./src/assets');
+	// const data = getFilesInsideDirectory('../frontend/src/assets');
+	res.send(data);
+});
 
-// function getFilesInsideDirectory(directory) {
-// 	const filesArray = [];
+function getFilesInsideDirectory(directory) {
+	const filesArray = [];
+	console.log(__dirname);
+	function readPath(directory) {
+		const dirRead = fs.readdirSync(directory);
 
-// 	function readPath(directory) {
-// 		const dirRead = fs.readdirSync(directory);
+		for (const file of dirRead) {
+			const absolutePath = path.join(directory, file);
 
-// 		for (const file of dirRead) {
-// 			const absolutePath = path.join(directory, file);
+			if (fs.statSync(absolutePath).isDirectory()) {
+				readPath(absolutePath);
+			} else {
+				filesArray.push(absolutePath);
+			}
+		}
+	}
 
-// 			if (fs.statSync(absolutePath).isDirectory()) {
-// 				readPath(absolutePath);
-// 			} else {
-// 				filesArray.push(absolutePath);
-// 			}
-// 		}
-// 	}
+	readPath(directory);
 
-// 	readPath(directory);
-
-// 	return filesArray;
-// }
+	return filesArray;
+}
 
 app.listen(port, () => console.log(`Ouvindo na porta ${port} ...`));
