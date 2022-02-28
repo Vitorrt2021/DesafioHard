ebee45df2a1e26c4e88f0a7245f1ca52b989454b
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-02-28 01:35:05 -0400

    Tela de ranking praticamente terminada, ainda não está responsivo em alguns celulares.

--- DesafioHard/frontend/src/requests/ranking.js
+++ DesafioHard/frontend/src/requests/ranking.js
@@ -3,26 +3,11 @@ const apiURL = 'http://localhost:5000';
 
 const renderRanking = async () => {
 	//FIX-IT: IMPROVE ERROR HANDLING
-
 	try {
 		const response = await $.ajax(apiURL + '/ranking');
 
 		renderRankingModal();
-
-		response.data.slice(0, 63).forEach((userData, index) => {
-			$('.ranking__cell__container').append(
-				`<div class="ranking__cell">
-						<div class="ranking__cell__rank">${index + 1}º</div>
-						<div class="ranking__cell__name_score_date_container">
-							<div class="ranking__cell__only_score_date_container">
-								<div class="ranking__cell__score">${userData.score}</div>
-								<div class="ranking__cell__date">${userData.date}</div>
-							</div>
-							<div class="ranking__cell__name">${userData.name}</div>
-						</div>
-				 </div>`
-			);
-		});
+		renderCells(response.data.slice(0, 63));
 	} catch (error) {
 		alert('check console for errors');
 		console.error(error);
@@ -56,6 +41,44 @@ const renderRankingModal = () => {
 			$('.modal')[0].style.display = 'none';
 		}
 	});
+};
+
+const renderCells = (ajaxResponse) => {
+	let index;
+
+	for (index = 0; index < 3; index++) {
+		const userData = ajaxResponse[index];
+
+		$('.ranking__cell__container').append(
+			`<div class="ranking__cell_${index + 1}">
+					<div class="ranking__cell__rank"></div>
+					<div class="ranking__cell__name_score_date_container">
+						<div class="ranking__cell__only_score_date_container">
+							<div class="ranking__cell__score">${userData.score}</div>
+							<div class="ranking__cell__date">${userData.date}</div>
+						</div>
+						<div class="ranking__cell__name">${userData.name}</div>
+					</div>
+			 </div>`
+		);
+	}
+
+	for (index = 3; index < ajaxResponse.length; index++) {
+		const userData = ajaxResponse[index];
+
+		$('.ranking__cell__container').append(
+			`<div class="ranking__cell">
+					<div class="ranking__cell__rank">${index + 1}º</div>
+					<div class="ranking__cell__name_score_date_container">
+						<div class="ranking__cell__only_score_date_container">
+							<div class="ranking__cell__score">${userData.score}</div>
+							<div class="ranking__cell__date">${userData.date}</div>
+						</div>
+						<div class="ranking__cell__name">${userData.name}</div>
+					</div>
+			 </div>`
+		);
+	}
 };
 
 export default renderRanking;
